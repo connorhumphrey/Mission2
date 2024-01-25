a57f9011b47aae306eed33fb0a76165d896839d8
connorhumphrey <connorhumphrey@CONNORHUMPHD67C>
2024-01-24 21:55:01 -0700

    Add project files.

---
+++ DiceRoller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class DiceRoller // randomly roll the dice
+    {
+        private readonly Random random = new Random();
+
+        public int[] SimulateRolls(int numRolls)
+        {
+            int[] results = new int[13]; // 
+
+            for (int i = 0; i < numRolls; i++)
+            {
+                int dice1 = random.Next(1, 7); //roll twice
+                int dice2 = random.Next(1, 7);
+                int sum = dice1 + dice2; 
+
+                results[sum]++; //append array
+            }
+
+            return results;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Mission2
+{
+    class DiceSimulator
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            
+            Console.Write("How many dice rolls would you like to simulate? "); //prompt user
+            string input = Console.ReadLine()!; // Adding ! to show non-null
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                int numRolls = int.Parse(input);
+
+                //run the simulation
+                DiceRoller diceRoller = new DiceRoller();
+                int[] results = diceRoller.SimulateRolls(numRolls);
+
+                //display results
+                Console.WriteLine("\n--SIMULATION RESULTS--");
+                Console.WriteLine("Each \"*\" represents 1% of the total number of rolls\n");
+
+                //show graph
+                for (int i = 2; i <= 12; i++)
+                {
+                    int percentage = (results[i] * 100 + numRolls / 2) / numRolls;
+                    string asterisks = new string('*', percentage);
+                    Console.WriteLine($"{i}: {asterisks}");
+                }
+
+                Console.WriteLine($"\nTotal number of rolls: {numRolls}.");
+                Console.WriteLine("Thank you for using the dice throwing simulator!");
+            }
+            //alert if user enters nothing
+            else
+            {
+               Console.WriteLine("Invalid input. Please enter a valid number of rolls.");
+            }
+        }
+    }
+}
